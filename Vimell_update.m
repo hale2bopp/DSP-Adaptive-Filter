ee6708ac5e85d3fcc7967ac6de4c0b33b5fe403b
Vimell Presad Yuvarajah <vimellyuvarajah@huntsman-ve501-0750.apn.wlan.upenn.edu>
2018-04-19 19:34:18 -0400

    QA and QB working

--- Vimell_update.m
+++ Vimell_update.m
@@ -124,49 +124,47 @@ hold off;
 %% changing frequency
 
 k = 1e-5;
-
+N  = 20000;
+x  = zeros(1,N);
 for n = 1:N
-    x = 100*cos(2*pi*(0.5+k*n)*n);
+    x(n) = 100*cos(2*pi*(0.5+k*n)*n);
 end
 
 
 %% cascade notch
-
+N= 20000;
 m = 1:N;
-w=pi;
-input = cos(w*m);
 
-w_noise_1 = 0.2*pi; 
-noise_1 = 100*cos(w_noise*m); 
-
-w_noise_2 = 0.6*pi; 
-noise_2 = 100*cos(w_noise*m); 
+input = cos(pi*m); 
+noise_1 = 100*cos(0.2*pi*m); 
+noise_2 = 100*cos(0.6*pi*m); 
 
 x = input + noise_1 + noise_2;
 
-mu = 1e-7;
+mu = 1e-6;
+r = 0.98;
 
 a1 = zeros(1,N);
 e1 = zeros(1,N);
-y1 =zeros(1,N);
+y1 = zeros(1,N);
 
 a2 = zeros(1,N);
 e2 = zeros(1,N);
-y2 =zeros(1,N);
+y2 = zeros(1,N);
+
 
 for n=3:N
     e1(n) = x(n) + a1(n)*x(n-1) + x(n-2);
-    y1(n) = e1(n) - r*a1(n)*y1(n-1) - (r^2)*y1(n-2);
+    y1(n) = e1(n) - r*a1(n)*y1(n-1) - r*r*y1(n-2);
+    e2(n) = y1(n) + a2(n)*y1(n-1) + y1(n-2);
+    y2(n) = e2(n) - r*a2(n)*y2(n-1) - r*r*y2(n-2);
     
     a1(n+1) = a1(n) - mu*y2(n)*x(n-1);
     
     if (abs(a1(n+1))>2)
         a1(n+1) = 0;
     end
-    
-    e2(n) = y1(n) + a2(n)*y1(n-1) + y1(n-2);
-    y2(n) = e2(n) - r*a2(n)*y2(n-1) - (r^2)*y2(n-2);
-    
+ 
     a2(n+1) = a2(n) - mu*y2(n)*y1(n-1);
     
     if (abs(a2(n+1))>2)
@@ -175,54 +173,169 @@ for n=3:N
     
 end
 
+figure; plot(a1);
+figure; plot(a2);
+
 
 %% Q B
 
-x = sign(rand(1, 1000)-0.5);
+N = 5000;
+
+train = sign(rand(1, N)-0.5);
+train = 20*train;
+
 h = [0.3,1,0.7,0.3,0.2];
-x = awgn(conv(x,h), 25);
+noise = randn( 1, length(conv(h,train)) );
+x = conv(h, train) + noise;
+
 
 d = 2;
 M = 20;
-N = 20000;
 adapt_filter = zeros(1,M+1);
-% what is mu?
-mu = 1e-7;
+adapt_filter(M/2) = 1;
+mu = 1e-6;
 error = zeros(1,N);
 y = zeros(1,N);
-for k=21:N
+
+for k=M+1:N
     for indx = 1:M+1
         y(k) = y(k) + x(k+1-indx)*adapt_filter(indx);
     end
-    error(k) = w(k-d) - y(k);
+    error(k) = train(k-d) - y(k);
     for indx = 1:M+1
-        adapt_filter(indx+1) = adapt_filter(indx) + 2*mu*error(k)*x(k+1-indx);
+        adapt_filter(indx) = adapt_filter(indx) + 2*mu*error(k)*x(k+1-indx);
     end
 end
+
+figure; plot(error);
+figure; freqz(h,1);
+figure; freqz(adapt_filter,1);
+
+
 
 
 %% Q B Part 2
 
-x = sign(rand(1, 1000)-0.5);
+N = 100000;
+
+train = sign(rand(1, N)-0.5);
+train = 100*train;
+
 h = [0.3,1,0.7,0.3,0.2];
-x = awgn(conv(x,h), 25);
 
-d = 2;
+%noise = randn( 1, length(conv(h,train)))/10;
+noise = randn( 1, length(conv(h,train)));
+x = conv(h, train) + noise;
+
 M = 20;
-N = 20000;
 adapt_filter = zeros(1,M+1);
-% what is mu?
-mu = 1e-7;
+adapt_filter(M/2) = 1;
+y = zeros(1,N);
+%y(M/2) = 0.1;
+error = zeros(1,N);
+mu = 1e-11;
 
-for k=21:N
+for k=M+1:N
     for indx = 1:M+1
         y(k) = y(k) + x(k+1-indx)*adapt_filter(indx);
     end
-    error = y(k)*y(k) - 1 ;
+    error(k) = y(k)*y(k) - 100*100 ;
     for indx = 1:M+1
-        adapt_filter(indx+1) = adapt_filter(indx) - mu*error(k)*y(k)*x(k+1-indx);
+        adapt_filter(indx) = adapt_filter(indx) - mu*error(k)*y(k)*x(k+1-indx);
     end
 end
+
+figure; plot(error);
+figure; freqz(h,1);
+figure; freqz(adapt_filter,1);
+
+%% QC
+
+%n1 = 0:5000;
+
+L = 10;
+N = 50000;
+n = 1:N;
+freq = linspace(0, 0.5, L);
+x = zeros(1,N);
+for f = freq
+    x = x + cos(f*pi*n);
+end
+
+f = 0:0.00001:0.5;
+
+Hd = 2*(f>=0 & f<=0.15) + 1*(f>=0.3 & f<= 0.5);
+
+for n2 = 1:N
+    Hd(n2) = abs(Hd(n2))*exp(1i*n2*pi/20);
+end
+
+hd = ifft(Hd);
+conv_h = conv(hd, x);
+
+M = 40;
+N = 50000;
+adapt_filter = zeros(1, M+1);
+adapt_filter(M/2) = 1;
+y = zeros(1,N);
+mu = 1e-7;
+
+for k=M+1:N
+    for indx = 1:M+1
+        y(k) = y(k) + x(k+1-indx)*adapt_filter(indx);
+    end
+    error(k) = abs(conv_h(k) - y(k));
+    for indx = 1:M+1
+        adapt_filter(indx) = adapt_filter(indx) + 2*mu*error(k)*x(k+1-indx);
+    end
+end
+
+figure; plot(error);
+figure; freqz(adapt_filter,1);
+
+%% QC part 2
+
+
+L = 10;
+
+freq = linspace(0, 0.5, L);
+x = zeros(1,N);
+N = 50000;
+n = 0:N;
+for f = freq
+    x = x + 100*cos((pi+f)*n);
+end
+
+f = 0:0.0001: 0.5;
+
+Hd = 2*pi*f*(f>=0.0 & f<=0.30);
+
+for n2 = 1:N
+    Hd(n2) = abs(Hd(n2))*exp(1i*(n2*pi/20 + pi/2));
+end
+
+hd = ifft(Hd);
+conv_h = conv(hd, x);
+
+M = 40;
+N = 50000;
+adapt_filter = zeros(1, M+1);
+adapt_filter(M/2) = 1;
+y = zeros(1,N);
+mu = 1e-7;
+
+for k=M+1:N
+    for indx = 1:M+1
+        y(k) = y(k) + x(k+1-indx)*adapt_filter(indx);
+    end
+    error(k) = abs(conv_h(k) - y(k));
+    for indx = 1:M+1
+        adapt_filter(indx) = adapt_filter(indx) + 2*mu*error(k)*x(k+1-indx);
+    end
+end
+
+
+
 
 
 
